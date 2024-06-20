74ebd61933c92a99942d82b13cc92ebd95baafad
Deadknight <1907606+Deadknight@users.noreply.github.com>
2024-06-20 15:57:21 +0300

    *Updated releases

--- src/views/Releases.vue
+++ src/views/Releases.vue
@@ -4,15 +4,18 @@
         <div style="font-size:medium; margin-bottom:40px; border-top:1px solid black;" class="nocontent">
         </div>
         <p>All release notes can be found through the Github and can be viewed below for your convenience.</p>
+        <a href="http://github.com/topping-dev/topping-kotlin" target="_blank">ToppingEngine Kotlin Multiplatform</a><br/>
+            <p><a :href="releasesKotlin[0].html_url" target="__blank"></a></p>
+        <vue-markdown-plus>{{readmeKotlin}}</vue-markdown-plus>
         <OSLangSelector v-model="langos" :showKotlin="false" :showLua="false">
             <template v-slot:Lua-Android>
                 <a href="http://github.com/topping-dev/topping-android" target="_blank">ToppingEngine Android</a><br/>
-                <p><a :href="releasesAndroid[0].html_url" target="__blank">{{releasesAndroid[0].name}}</a></p>
+                <p><a :href="releasesAndroid[0].html_url" target="__blank"></a></p>
                 <vue-markdown-plus>{{readmeAndroid}}</vue-markdown-plus>
             </template>
             <template v-slot:Lua-iOS>
                 <a href="http://github.com/topping-dev/topping-ios" target="_blank">ToppingEngine iOS</a><br/>
-                <p><a :href="releasesIos[0].html_url" target="__blank">{{releasesIos[0].name}}</a></p>
+                <p><a :href="releasesIos[0].html_url" target="__blank"></a></p>
                 <vue-markdown-plus>{{readmeIos}}</vue-markdown-plus>
             </template>
         </OSLangSelector>
@@ -36,6 +39,8 @@ export default {
     },
     data: () => ({
         langos: "Lua-Android",
+        releasesKotlin: {},
+        readmeKotlin: "",
         releasesIos: {},
         readmeIos: "",
         releasesAndroid: {},
@@ -43,6 +48,25 @@ export default {
     }),
     mounted() {
         vm = this;
+
+        var xhrkotlin = new XMLHttpRequest();
+        xhrkotlin.onreadystatechange = function() {
+            if (xhrkotlin.readyState === 4) {
+                var responseObject = JSON.parse(xhrkotlin.responseText);
+                vm.$data.releasesKotlin = responseObject;
+            }
+        };
+        xhrkotlin.open('GET', 'https://api.github.com/repos/topping-dev/topping-kotlin/releases');
+        xhrkotlin.send()
+
+        var xhrkotlinreadme = new XMLHttpRequest();
+        xhrkotlinreadme.onreadystatechange = function() {
+            if (xhrkotlinreadme.readyState === 4) {
+                vm.$data.readmeKotlin = xhrkotlinreadme.responseText;
+            }
+        };
+        xhrkotlinreadme.open('GET', 'https://raw.githubusercontent.com/topping-dev/topping-kotlin/master/README.md');
+        xhrkotlinreadme.send()
 
         var xhrios = new XMLHttpRequest();
         xhrios.onreadystatechange = function() {
